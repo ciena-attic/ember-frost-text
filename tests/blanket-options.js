b482c04ed2304f4c9ef16dcec7d250bd74f3baaf
Matthew Dahl <mdahl@ciena.com>
2016-03-08 15:56:39 -0800

    Add extension

--- tests/blanket-options.js
+++ tests/blanket-options.js
@@ -12,7 +12,7 @@ var options = {
     lcovOptions: {
       outputFile: 'coverage/lcov.info',
       renamer: function (fileName) {
-        return fileName.replace('ember-frost-text', 'ember-frost-text/addons')
+        return fileName.replace('ember-frost-text', 'ember-frost-text/addons') + '.js'
       }
     }
   }
