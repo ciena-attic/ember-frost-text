c0dd7b1e6683470b4452d5f0cec5e216a1c2ea5a
Matthew Dahl <mdahl@ciena.com>
2016-03-08 14:26:05 -0800

    Use ember-cli-blanket for code coverage

---
+++ tests/blanket-options.js
@@ -0,0 +1,18 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'ember-frost-text',
+  filter: '//.*ember-frost-text/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['json'],
+    autostart: true
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
