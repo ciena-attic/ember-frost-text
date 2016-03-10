674f0feff3e8edc8ba400170dd24a240a2facbcf
Matthew Dahl <mdahl@ciena.com>
2016-03-10 08:58:13 -0800

    travis debugging

--- .travis/publish-coverage.sh
+++ .travis/publish-coverage.sh
@@ -3,6 +3,9 @@
 STABLE_NODE=`nvm alias stable | awk '{ print $5; }' | sed -e "s/)//"`
 CURRENT_NODE=`node -v`
 
+echo "STABLE_NODE = ${STABLE_NODE}"
+echo "CURRENT_NODE = ${CURRENT_NODE}"
+
 if [ "$EMBER_TRY_SCENARIO" != "default" ]
 then
   echo "Skipping coverage publish for EMBER_TRY_SCENARIO ${EMBER_TRY_SCENARIO}"
