7dc61eae5db23bf62a8ef531eb547eeb6f27b94c
Matthew Dahl <mdahl@ciena.com>
2016-03-10 09:09:14 -0800

    more debugging

--- .travis/publish-coverage.sh
+++ .travis/publish-coverage.sh
@@ -3,6 +3,7 @@
 STABLE_NODE=`nvm alias stable | awk '{ print $5; }' | sed -e "s/)//"`
 CURRENT_NODE=`node -v`
 
+nvm alias
 echo "STABLE_NODE = ${STABLE_NODE}"
 echo "CURRENT_NODE = ${CURRENT_NODE}"
 
