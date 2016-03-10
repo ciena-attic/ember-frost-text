1dae5ff9f2333d4fa61dd1558c68418fc2cadfbc
Matthew Dahl <mdahl@ciena.com>
2016-03-10 08:26:28 -0800

    improve coverage script

--- .travis/publish-coverage.sh
+++ .travis/publish-coverage.sh
@@ -1,7 +1,16 @@
 STABLE_NODE=nvm alias stable | awk '{ print $5; }' | sed -e "s/)//"
 CURRENT_NODE=node -v
 
-if [ "$EMBER_TRY_SCENARIO" = 'default' && "$CURRENT_NODE" = "$STABLE_NODE" ]
+if [ "$EMBER_TRY_SCENARIO" != "default" ]
 then
-  cat coverage/lcov.info | coveralls
+  echo "Skipping coverage publish for EMBER_TRY_SCENARIO ${EMBER_TRY_SCENARIO}"
+  exit 0
 fi
+
+if [ "$CURRENT_NODE" != "$STABLE_NODE" ]
+then
+  echo "Skipping coverage publish for node ${CURRENT_NODE} since it is not stable (${STABLE_NODE})"
+  exit 0
+fi
+
+cat coverage/lcov.info | coveralls
